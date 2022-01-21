bb516cd41aee006b6cd5087c5b676f351de7f3d3
mohamedtamer0 <mt4452242@gmail.com>
2022-01-21 18:27:34 +0200

    update

---
+++ app/src/main/java/com/example/collapsebottomnavigation_jetpackcompose/navigation/BottomScreen.kt
@@ -0,0 +1,7 @@
+package com.example.collapsebottomnavigation_jetpackcompose.navigation
+
+import androidx.compose.ui.graphics.vector.ImageVector
+
+sealed class BottomScreen(val route: String, val title: String, val icon: ImageVector) {
+
+}
